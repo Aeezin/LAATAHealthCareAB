3b8e85111b758abacd354eda156cf632f16de18c
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-08 12:21:07 +0100

    Create ApplicationUser: - int-based ID - add custom fields: CreatedAt and UpdatedAt

--- HealthCareAB_v1/HealthCareAB_v1/Models/Entites/ApplicationUser.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Models/Entites/ApplicationUser.cs
@@ -1,1 +1,11 @@
-﻿namespace HealthCareAB_v1.Models.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HealthCareAB_v1.Models.Entities;
+
+public class ApplicationUser : IdentityUser<int>
+{
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    // TODO: Navigation properties
+}
