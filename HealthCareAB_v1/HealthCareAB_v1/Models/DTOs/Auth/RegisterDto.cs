3631e360e48918d536edd025069505f5fb3ac3d5
Aeezin <gullestrand@gmail.com>
2026-01-09 12:29:41 +0100

    Refactored - Added needed fields to RegisterDTO

--- HealthCareAB_v1/HealthCareAB_v1/Models/DTOs/Auth/RegisterDto.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Models/DTOs/Auth/RegisterDto.cs
@@ -1,31 +1,44 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 namespace HealthCareAB_v1.DTOs
 {
     public class RegisterDto
     {
-        [Required(ErrorMessage = "Username is required")]
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(
+            @"^[A-Za-zÅÄÖåäö]{1,20}(-[A-Za-zÅÄÖåäö]{1,20})*$",
+            ErrorMessage = "Name may contain letters and hyphens only"
+        )]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Lastname is required")]
+        [RegularExpression(@"^[A-Za-zÅÄÖåäö]{2,20}$")]
+        public string Lastname {get; set;} = null!;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         [StringLength(
             50,
-            MinimumLength = 3,
-            ErrorMessage = "Username must be between 3 and 50 characters"
-        )]
-        public string Username { get; set; }
+            MinimumLength = 5)]
+        public string Email {get; set;} = null!;
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [RegularExpression(@"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$")]
+        public string DateOfBirth {get; set;} = null!;
 
         [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(
+            @"^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$",
+            ErrorMessage = "Password must contain at least 1 capital letter, 1 number and 1 special character")]
         [StringLength(
             100,
-            MinimumLength = 6,
-            ErrorMessage = "Password must be at least 6 characters"
-        )]
-        public string Password { get; set; }
+            MinimumLength = 8,
+            ErrorMessage = "Password must be at least 8 characters long")]
+        public string Password { get; set; } = null!;
 
-        /// // <summary>
-        /// Optional roles for the new user.
-        /// Note: Admin role can be assigned manually through Swagger. This is ok in dev, in the future this should
-        /// be changed to a more solid sulotion. For now you can leave it as it is if you want.
-        /// Non-admin requests with Admin role will be ignored (defaults to User).
-        /// </summary>
-        public List<string>? Roles { get; set; }
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number is invalid")]
+        public string? PhoneNumber { get; set; }
     }
 }
