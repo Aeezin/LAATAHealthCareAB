2f1b5d3325b28f1556b93aaee2645865ddcb2e92
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-11 10:46:48 +0100

    Add Get methods to repository interface

--- HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/ICaregiverScheduleRepository.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/ICaregiverScheduleRepository.cs
@@ -5,5 +5,8 @@ namespace HealthCareAB_v1.Repositories.Interfaces;
 public interface ICaregiverScheduleRepository
 {
     Task<CaregiverSchedule> CreateAsync(CaregiverSchedule schedule);
+    Task<CaregiverSchedule?> GetByIdAsync(int id);
+    Task<List<CaregiverSchedule>> GetByCaregiverIdAsync(int caregiverId);
+
     Task<bool> HasOverlappingScheduleAsync(int caregiverId, DayOfWeek dayOfWeek, TimeOnly startTime, TimeOnly endTime);
 }
