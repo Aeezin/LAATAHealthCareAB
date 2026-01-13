da7bec4c6c485cd3dfd8cf4ae0f5be68c0397246
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-13 10:08:22 +0100

    Add response dto

---
+++ HealthCareAB_v1/HealthCareAB_v1/Models/DTOs/Appointment/AppointmentResponse.cs
@@ -0,0 +1,20 @@
+using HealthCareAB_v1.Models.Enums;
+
+namespace HealthCareAB_v1.Models.DTOs;
+
+public class AppointmentResponse
+{
+    public int Id { get; set; }
+
+    public required int PatientId { get; set; }
+    public required int CaregiverId { get; set; }
+
+    public required DateOnly Date { get; set; }
+    public required TimeOnly StartTime { get; set; }
+    public required TimeOnly EndTime { get; set; }
+
+    public string? PatientNotes { get; set; }
+    public string? CaregiverNotes { get; set; }
+
+    public AppointmentStatus Status { get; set; }
+}
