fccc478aa474afa785a4cb005addfb6ba6843123
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-13 13:31:26 +0100

    Add business rule validation methods to repository layer

--- HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
@@ -5,4 +5,11 @@ namespace HealthCareAB_v1.Repositories.Interfaces;
 public interface IAppointmentRepository
 {
     Task<Appointment> CreateAsync(Appointment appointment);
+
+    Task<int> GetPatientBookingCountInLast30DaysAsync(int patientId, DateOnly fromDate);
+    Task<bool> HasConflictingAppointmentAsync(
+        int caregiverId,
+        DateOnly date,
+        TimeOnly startTime,
+        TimeOnly endTime);
 }
