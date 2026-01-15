17d604df60bb8add651fe9d04d1742f0c2fdab94
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-15 08:44:59 +0100

    Rename method names in interface

--- HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
@@ -5,8 +5,10 @@ namespace HealthCareAB_v1.Repositories.Interfaces;
 public interface IAppointmentRepository
 {
     Task<Appointment> CreateAsync(Appointment appointment);
+    Task<List<Appointment>> GetByPatientIdAsync(int patientId);
+    Task<List<Appointment>> GetByCaregiverIdAsync(int caregiverId);
 
-    Task<int> GetPatientBookingCountInLast30DaysAsync(int patientId, DateOnly fromDate);
+    Task<int> GetPatientAppointmentCountInLast30DaysAsync(int patientId, DateOnly fromDate);
     Task<bool> HasConflictingAppointmentAsync(
         int caregiverId,
         DateOnly date,
