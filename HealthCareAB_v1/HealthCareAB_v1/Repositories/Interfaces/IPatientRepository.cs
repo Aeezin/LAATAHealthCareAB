045a7241ff57ed17580a34815ef83287f415fb2d
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-15 08:45:27 +0100

    Add GetByUserIdAsync sig to interface

--- HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IPatientRepository.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IPatientRepository.cs
@@ -5,4 +5,5 @@ namespace HealthCareAB_v1.Repositories.Interfaces;
 public interface IPatientRepository
 {
     Task<Patient?> GetByPersonalIdentityNumberAsync(string personalIdentityNumber);
+    Task<Patient?> GetByUserIdAsync(int userId);
 }
