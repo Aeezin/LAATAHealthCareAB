88e3535ed463ae46d863531db361bd887832defd
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-15 08:43:25 +0100

    Add Get method sig to interface

--- HealthCareAB_v1/HealthCareAB_v1/Services/Interfaces/IAppointmentService.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Services/Interfaces/IAppointmentService.cs
@@ -5,4 +5,5 @@ namespace HealthCareAB_v1.Services.Interfaces;
 public interface IAppointmentService
 {
     Task<Appointment> CreateAsync(Appointment appointment);
+    Task<List<Appointment>> GetByUserIdAsync(int userId);
 }
