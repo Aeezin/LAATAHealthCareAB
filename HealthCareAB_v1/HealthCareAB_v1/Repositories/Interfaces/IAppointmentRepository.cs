316de975ab26ecb9e33bc91735618033110d20db
LudwigStenberg <lludwigstenberg@gmail.com>
2026-01-18 09:36:19 +0100

    Add new repository interface method

--- HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
+++ HealthCareAB_v1/HealthCareAB_v1/Repositories/Interfaces/IAppointmentRepository.cs
@@ -7,6 +7,7 @@ public interface IAppointmentRepository
     Task<Appointment> CreateAsync(Appointment appointment);
     Task<List<Appointment>> GetByPatientIdAsync(int patientId);
     Task<List<Appointment>> GetByCaregiverIdAsync(int caregiverId);
+    Task<List<Appointment>> GetByCaregiverAndDateRangeAsync(int caregiverId, DateTime startDate, DateTime endDate);
 
     Task<int> GetPatientAppointmentCountInLast30DaysAsync(int patientId, DateOnly fromDate);
     Task<bool> HasConflictingAppointmentAsync(
